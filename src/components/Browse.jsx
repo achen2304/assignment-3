3a38fd7365b6ecf7949f8ec66edce58beedf6314
Mei <mkchng@iatate.edu>
2025-04-14 08:47:18 -0500

    adding course info to browse page

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -7,15 +7,30 @@ const Browse = ({ cart, setCart, setStep }) => {
   // - Add search input fields (extra credit)
   // - Add "Add to Cart" button for each course
 
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-6"></div>
+
+  const listItems = Courses.map((item) => (
+          <div key={item.offering_id} >
+            
+          <img className="img-fluid" src={item.image}
+          width={350} /> <br/>
+          <h1 className="flex justify-center text-xl"> {item.title}</h1>
+          {item.category} <br/>
+          {item.price} <br/>
+          </div>
+          ));
+
   return (
     <div>
       {/* Add appropriate Tailwind styling 👇 */}
-      <h2>Browse Courses</h2> 
+      <h2 className='flex justify-center'>Browse Courses</h2> 
+      {listItems}
       {/* Search input (Extra Credit)*/} 
       {/* Course list with Add to Cart button */}
       <button onClick={() => setStep("cart")}>Go to Cart</button>
     </div>
   );
+  
 };
 
 export default Browse;
