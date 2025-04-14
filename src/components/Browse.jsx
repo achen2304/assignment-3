4379890bb742faf37f6cfd104fa89f6c577c5a47
Mei <mkchng@iatate.edu>
2025-04-14 08:59:24 -0500

    tailwind fomatting course info

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -7,14 +7,11 @@ const Browse = ({ cart, setCart, setStep }) => {
   // - Add search input fields (extra credit)
   // - Add "Add to Cart" button for each course
 
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-6"></div>
-
   const listItems = Courses.map((item) => (
           <div key={item.offering_id} >
             
-          <img className="img-fluid" src={item.image}
-          width={350} /> <br/>
-          <h1 className="flex justify-center text-xl"> {item.title}</h1>
+          <img className="img-fluid" src={item.image} width={350} /> <br/>
+          <h1 className="flex item-center text-xl"> {item.title}</h1>
           {item.category} <br/>
           {item.price} <br/>
           </div>
@@ -23,14 +20,15 @@ const Browse = ({ cart, setCart, setStep }) => {
   return (
     <div>
       {/* Add appropriate Tailwind styling 👇 */}
-      <h2 className='flex justify-center'>Browse Courses</h2> 
-      {listItems}
+      <h2 className='flex justify-center font-bold'>Browse Courses</h2> 
+      <div className='grid grid-cols-3 gap-{4}'>
+      {listItems }
+      </div>
       {/* Search input (Extra Credit)*/} 
       {/* Course list with Add to Cart button */}
       <button onClick={() => setStep("cart")}>Go to Cart</button>
     </div>
   );
-  
 };
 
 export default Browse;
