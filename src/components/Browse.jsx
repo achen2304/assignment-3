ccacf026f20cf9b0f1565859a867d39019dcb2d7
Mei <mkchng@iatate.edu>
2025-04-14 09:39:45 -0500

    added courses to browse

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -8,21 +8,20 @@ const Browse = ({ cart, setCart, setStep }) => {
   // - Add "Add to Cart" button for each course
 
   const listItems = Courses.map((item) => (
-          <div key={item.offering_id} >
-            
-          <img className="img-fluid" src={item.image} width={350} /> <br/>
+        <div key={item.offering_id}>
+          <img className="img-fluid" src={item.image} width={350}/> <br/>
           <h1 className="flex justify-center item-center text-xl "> {item.title}</h1>
           {item.category} <br/>
           {item.price} <br/>
-          </div>
-          ));
+        </div>
+      ));
 
   return (
     <div>
       {/* Add appropriate Tailwind styling 👇 */}
-      <h2 className='flex justify-center font-bold '>Browse Courses</h2> 
-      <div className='grid grid-cols-3 gap-{4}'>
-      {listItems }
+      <h2 className='flex-wrap justify-center font-bold'>Browse Courses</h2> 
+      <div className='grid grid-cols-3 gap-{4px}'>
+        {listItems }
       </div>
       {/* Search input (Extra Credit)*/} 
       {/* Course list with Add to Cart button */}
