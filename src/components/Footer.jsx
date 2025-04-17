b6e80f9fae3ce914974716d1ad046a07a2a1dc65
Chen, Cai <achen23@iastate.edu>
2025-04-17 20:10:04 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-background text-white py-4 fixed bottom-0 w-full z-10">
-      <div className="container mx-auto px-4">
-        <p className="text-center">© 2025 Your Company. All rights reserved.</p>
+    <footer className="bg-secondary text-text p-4 mt-auto">
+      <div className="max-w-6xl mx-auto text-center">
+        <h1 className="pb-2 font-bold">
+          &copy; Spring 2025 Construction of User Interfaces
+        </h1>
+        <div className="text-sm">
+          <p>Megan Chng implemented the Courses & Order Summary views.</p>
+          <p>Cai Chen implemented the Cart and Payment gateway views.</p>
+        </div>
       </div>
     </footer>
   );
