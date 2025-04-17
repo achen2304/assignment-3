7232ecd6a8257f7c01bb63fa13bce22426d0f724
Mei <mkchng@iatate.edu>
2025-04-17 16:02:24 -0500

    added buttons to add and remove courses

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -8,19 +8,36 @@ const Browse = ({ cart, setCart, setStep }) => {
   // - Add "Add to Cart" button for each course
 
   const listItems = Courses.map((item) => (
+    <div class= "container mx-auto px-4">
         <div key={item.offering_id}>
-          <img className="img-fluid" src={item.image} width={350}/> <br/>
-          <h1 className="flex justify-center item-center text-xl "> {item.title}</h1>
+          <img className="img-fluid" src={item.image} /> <br/>
+          <h1 className="flex text-xl "> {item.title}</h1>
           {item.category} <br/>
           {item.price} <br/>
+          <button type="button" variant="dark" onClick={() => removeFromCart(item)} > Remove </button>{" "}
+          <button type="button" variant="dark" onClick={() => addToCart(item)}> Add </button>
+
+          </div>
         </div>
       ));
+
+      const removeFromCart = (item) => {
+        let hardCopy = [...cart];
+        hardCopy = hardCopy.filter((cartItem) => cartItem.id !== item.id);
+        setCart(hardCopy);
+    };
+
+    const addToCart = (item) => {
+      setCart([...cart, item]);
+  };
+
+
 
   return (
     <div>
       {/* Add appropriate Tailwind styling 👇 */}
-      <h2 className='flex-wrap justify-center font-bold'>Browse Courses</h2> 
-      <div className='grid grid-cols-3 gap-{4px}'>
+      <h2 className=' font-bold'>Browse Courses</h2> 
+      <div className='grid grid-cols-4'>
         {listItems }
       </div>
       {/* Search input (Extr Credit)*/} 
